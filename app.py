922f6e7c7785d38d1c6d71227a594e79e3e62d11
Jan Kühn <yotka@posteo.org>
2023-07-31 15:23:19 +0200

    Add possibility to share a graph via URL

--- app.py
+++ app.py
@@ -2,14 +2,205 @@
 Streamlit app.
 """
 
+import datetime as dt
+import urllib.parse
+
 import extra_streamlit_components as stx
 import folium
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
+from pydantic.v1.utils import deep_update
 from streamlit_folium import folium_static
 
 import utils
+
+
+def get_form_defaults() -> dict:
+    """
+    Get default values for the form.
+    """
+    form_defaults = {
+        "method": "by_name",
+        "lat": "",
+        "lon": "",
+        "display_name": "",
+        "year": dt.datetime.now().year,
+        "ref_period": (1961, 1990),
+        "highlight_max": 1,
+        "metric": "temperature_mean",
+        "units": "metric",
+        "smooth": 1,
+        "peak_method": "mean",
+        "peak_alpha": True,
+        "alternate_months": True,
+    }
+
+    return form_defaults
+
+
+def get_base_url() -> str:
+    """
+    Get base URL from current session.
+    """
+
+    session = st.runtime.get_instance()._session_mgr.list_active_sessions()[0]
+    base_url = urllib.parse.urlunparse(
+        [session.client.request.protocol, session.client.request.host, "", "", "", ""]
+    )
+
+    return base_url
+
+
+def get_query_params() -> dict:
+    """
+    Get query parameters from URL.
+    """
+
+    query = st.experimental_get_query_params()
+
+    allowed_params = [
+        "lat",
+        "lon",
+        "display_name",
+        "year",
+        "ref_period",
+        "highlight_max",
+        "metric",
+        "units",
+        "smooth",
+        "peak_method",
+        "peak_alpha",
+        "alternate_months",
+    ]
+
+    # Filter out values not allowed or empty
+    params = {
+        key: query[key]
+        for key in allowed_params
+        if key in query.keys() and query[key] != ""
+    }
+
+    # Convert parameters and apply sanity checks
+    remove_keys = []
+
+    for key, value in params.items():
+        # Check keys with float values
+        if key in ["lat", "lon"] and isinstance(value, list):
+            try:
+                params[key] = float(value[0])
+                if key == "lat" and (params[key] < -90 or params[key] > 90):
+                    remove_keys.append(key)
+                if key == "lon" and (params[key] < -180 or params[key] > 180):
+                    remove_keys.append(key)
+            except ValueError:
+                remove_keys.append(key)
+
+        # Check keys with int values
+        elif key in ["year", "highlight_max", "smooth"] and isinstance(value, list):
+            try:
+                params[key] = int(value[0])
+                if key == "year" and (
+                    params[key] < 1940 or params[key] > dt.datetime.now().year
+                ):
+                    remove_keys.append(key)
+                if key == "highlight_max" and not 0 <= params[key] <= 5:
+                    remove_keys.append(key)
+                if key == "smooth" and not 0 <= params[key] <= 3:
+                    remove_keys.append(key)
+            except ValueError:
+                remove_keys.append(key)
+
+        # Check ref_period
+        elif key == "ref_period" and isinstance(value, list):
+            params[key] = (
+                int("".join(value).split("-", maxsplit=1)[0]),
+                int("".join(value).split("-", maxsplit=1)[1]),
+            )
+            if not (
+                params[key][0] in [1941, 1951, 1961, 1971, 1981, 1991]
+                and params[key][1] - params[key][0] == 29
+            ):
+                remove_keys.append(key)
+
+        # Check boolean values
+        elif key in ["peak_alpha", "alternate_months"] and isinstance(value, list):
+            params[key] = " ".join(value).split("-", maxsplit=1)[0] != "false"
+
+        # Check units
+        elif key == "units":
+            params[key] = " ".join(value)
+            if params[key] != "imperial":
+                remove_keys.append(key)
+
+        # Check metric
+        elif key == "metric":
+            params[key] = " ".join(value)
+            if params[key] not in [
+                "temperature_min",
+                "temperature_max",
+                "precipitation_rolling",
+                "precipitation_cum",
+            ]:
+                remove_keys.append(key)
+
+        else:
+            params[key] = " ".join(value)
+
+    # Remove invalid keys
+    params = {key: value for key, value in params.items() if key not in remove_keys}
+
+    if "lat" in params.keys() and "lon" in params.keys():
+        params["method"] = "by_coords"
+        return params
+
+    return {}
+
+
+def create_share_url(params: dict) -> str:
+    """
+    Create URL with settings parameters to share graph.
+    """
+    params = params.copy()
+
+    allowed_keys = [
+        "lat",
+        "lon",
+        "display_name",
+        "year",
+        "ref_period",
+        "highlight_max",
+        "metric",
+        "units",
+        "smooth",
+        "peak_method",
+        "peak_alpha",
+        "alternate_months",
+    ]
+
+    # Change metric to str
+    params["metric"] = params["metric"]["name"]
+
+    # Filter out values not allowed, defaults, and empty values
+    params = {
+        key: params[key]
+        for key in allowed_keys
+        if key in params.keys()
+        and params[key] != get_form_defaults()[key]
+        and params[key] is not None
+    }
+
+    # Change ref_period to str
+    if "ref_period" in params.keys():
+        params["ref_period"] = f"{params['ref_period'][0]}-{params['ref_period'][1]}"
+
+    # Change boolean values to lowercase
+    if "peak_alpha" in params.keys():
+        params["peak_alpha"] = str(params["peak_alpha"]).lower()
+    if "alternate_months" in params.keys():
+        params["alternate_months"] = str(params["alternate_months"]).lower()
+
+    return f"{get_base_url()}/?{urllib.parse.urlencode(params)}"
 
 
 def build_location_by_name(location: str) -> tuple[float, float, str]:
@@ -31,7 +222,6 @@ def build_location_by_name(location: str) -> tuple[float, float, str]:
         return lat, lon, location_name
 
 
-@st.cache_data(show_spinner=False, ttl="1h")
 def build_location_by_coords(lat: float, lon: float, display_name: str) -> str:
     """
     Build location by coordinates.
@@ -52,10 +242,19 @@ def build_location_by_coords(lat: float, lon: float, display_name: str) -> str:
         return location
 
 
-def build_form(method: str = "by_name") -> dict:
+def build_form(method: str = "by_name", params: dict = None) -> dict:
     """
     Build form for input values.
     """
+    params_set = False
+
+    defaults = st.session_state["form_defaults"]
+
+    if isinstance(params, dict):
+        if len(params) > 0:
+            defaults = deep_update(defaults, params)
+            params_set = True
+
     form_values = {"method": method}
 
     with st.form("settings_form"):
@@ -64,13 +263,14 @@ def build_form(method: str = "by_name") -> dict:
             lat_col, lon_col = st.columns([1, 1])
 
             with lat_col:
-                form_values["lat"] = st.text_input("Latitude:")
+                form_values["lat"] = st.text_input("Latitude:", value=defaults["lat"])
 
             with lon_col:
-                form_values["lon"] = st.text_input("Longitude:")
+                form_values["lon"] = st.text_input("Longitude:", value=defaults["lon"])
 
             form_values["display_name"] = st.text_input(
                 "Display name:",
+                value=defaults["display_name"],
                 help="""In case no display name can be found for the
                 given lat/lon coordinates, this name will be used""",
             )
@@ -92,24 +292,29 @@ def build_form(method: str = "by_name") -> dict:
 
         with year_col:
             # Create input widget for year
+            years = list(range(dt.datetime.now().year, 1939, -1))
             form_values["year"] = st.selectbox(
-                "Year to show:",
-                list(range(2023, 1939, -1)),
+                "Year to show:", years, index=years.index(defaults["year"])
             )
 
         with ref_col:
             # Selectbox for reference period
+            periods_tuples = [
+                (1991, 2020),
+                (1981, 2010),
+                (1971, 2000),
+                (1961, 1990),
+                (1951, 1980),
+                (1941, 1970),
+            ]
+
+            # Create str representation of tuples
+            periods = [f"{period[0]}-{period[1]}" for period in periods_tuples]
+
             select_ref_period = st.selectbox(
                 "Reference period:",
-                [
-                    "1991-2020",
-                    "1981-2010",
-                    "1971-2000",
-                    "1961-1990",
-                    "1951-1980",
-                    "1941-1970",
-                ],
-                index=3,
+                periods,
+                index=periods_tuples.index(defaults["ref_period"]),
                 help="""
                     The reference period is used to calculate the historical average of
                     the daily values. The average is then used to compare the daily
@@ -119,19 +324,18 @@ def build_form(method: str = "by_name") -> dict:
                     """,
             )
 
-            # Convert selection to tuple
+            # Use tuples as saved values
             if select_ref_period:
-                form_values["ref_period"] = (
-                    int(select_ref_period.split("-")[0]),
-                    int(select_ref_period.split("-")[1]),
-                )
+                form_values["ref_period"] = periods_tuples[
+                    periods.index(select_ref_period)
+                ]
 
         # Number of peaks to annotate
         form_values["highlight_max"] = st.slider(
             "Peaks to be annotated:",
             min_value=0,
             max_value=5,
-            value=1,
+            value=defaults["highlight_max"],
             help="""
                 Number of maximum peaks to be annotated. If peaks are close together,
                 the text might overlap. In this case, reduce the number of peaks.
@@ -139,46 +343,65 @@ def build_form(method: str = "by_name") -> dict:
         )
 
         # Selector for metric
-        metrics = {
-            "Mean temperature": {
+        metrics = [
+            {
                 "name": "temperature_mean",
                 "data": "temperature_2m_mean",
             },
-            "Minimum temperature": {
+            {
                 "name": "temperature_min",
                 "data": "temperature_2m_min",
             },
-            "Maximum temperature": {
+            {
                 "name": "temperature_max",
                 "data": "temperature_2m_max",
             },
-            "Precipitation (Rolling average)": {
+            {
                 "name": "precipitation_rolling",
                 "data": "precipitation_sum",
             },
-            "Precipitation (Cumulated)": {
+            {
                 "name": "precipitation_cum",
                 "data": "precipitation_sum",
             },
-        }
-        selected_metric = st.selectbox("Metric:", list(metrics.keys()))
-        form_values["metric"] = metrics[selected_metric]
+        ]
+        metrics_names = [
+            "Mean temperature",
+            "Minimum temperature",
+            "Maximum temperature",
+            "Precipitation (Rolling average)",
+            "Precipitation (Cumulated)",
+        ]
+
+        selected_metric = st.selectbox(
+            "Metric:",
+            metrics_names,
+            index=next(
+                i
+                for i, metric_dict in enumerate(metrics)
+                if metric_dict["name"] == defaults["metric"]
+            ),
+        )
+        form_values["metric"] = metrics[metrics_names.index(selected_metric)]
 
         with st.expander("Advanced settings"):
             # Selection for unit system
-            units = {
-                "Metric System (°C, mm)": "metric",
-                "Imperial System (°F, In)": "imperial",
-            }
-            selected_units = st.selectbox("Units:", list(units.keys()))
-            form_values["units"] = units[selected_units]
+            units = ["metric", "imperial"]
+            units_names = ["Metric System (°C, mm)", "Imperial System (°F, In)"]
+
+            selected_units = st.selectbox(
+                "Units:",
+                units_names,
+                index=units.index(defaults["units"]),
+            )
+            form_values["units"] = units[units_names.index(selected_units)]
 
             # Slider to apply LOWESS smoothing
             form_values["smooth"] = st.slider(
                 "Smoothing:",
                 min_value=0,
                 max_value=2,
-                value=1,
+                value=defaults["smooth"],
                 help="""Degree of smoothing to apply to the historical data.
                 0 means no smoothing. The higher the value, the more smoothing
                 is applied. Smoothing is done using LOWESS (locally weighted
@@ -186,10 +409,13 @@ def build_form(method: str = "by_name") -> dict:
             )
 
             # Select method to calculate peaks
+            peak_methods = ["mean", "percentile"]
+            peak_methods_names = ["Historical mean", "95 percentile"]
+
             peak_method = st.radio(
                 "Peak method - Difference to:",
-                ["Historical mean", "95 percentile"],
-                index=0,
+                peak_methods_names,
+                index=peak_methods.index(defaults["peak_method"]),
                 help="""
                     Method to determine the peaks. Either the difference to the historical
                     mean or the difference to the 95 percentile. The percentile method focuses
@@ -197,14 +423,14 @@ def build_form(method: str = "by_name") -> dict:
                     difference to the historical average.
                     """,
             )
-            form_values["peak_method"] = (
-                "mean" if peak_method == "Historical mean" else "percentile"
-            )
+            form_values["peak_method"] = peak_methods[
+                peak_methods_names.index(peak_method)
+            ]
 
             # Checkbox to decide if peaks should be emphasized
             form_values["peak_alpha"] = st.checkbox(
                 "Emphasize peaks",
-                value=True,
+                value=defaults["peak_alpha"],
                 help="""
                     If checked, peaks that leave the gray area between the 5 and 95 
                     percentile will be highlighted more.
@@ -214,7 +440,7 @@ def build_form(method: str = "by_name") -> dict:
             # Checkbox to decide if months should have alternating background
             form_values["alternate_months"] = st.checkbox(
                 "Alternate months",
-                value=True,
+                value=defaults["alternate_months"],
                 help="""
                     If checked, the background color of months is alternated.
                     """,
@@ -223,7 +449,7 @@ def build_form(method: str = "by_name") -> dict:
         # Create button to start the analysis
         form_values["create_graph"] = st.form_submit_button("Create")
 
-        if form_values["create_graph"]:
+        if form_values["create_graph"] or params_set:
             return form_values
 
         return None
@@ -273,6 +499,9 @@ def process_form(form_values: dict) -> dict:
         )
         if form_values["location_name"] is None:
             return None
+
+    # Set share URL
+    st.session_state["share_url"] = create_share_url(form_values)
 
     # Calculate values for smoothing
     if form_values["smooth"] == 0:
@@ -389,6 +618,10 @@ st.set_page_config(page_title="Historical Meteo Graphs", layout="wide")
 with open("style.css", encoding="utf-8") as css:
     st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
 
+# Define default values for the form
+if "form_defaults" not in st.session_state:
+    st.session_state["form_defaults"] = get_form_defaults()
+
 col1, col2 = st.columns([1, 3])
 
 with col1:
@@ -398,7 +631,24 @@ with col1:
         unsafe_allow_html=True,
     )
 
+    # Create a placeholder for messages
     message_box = st.empty()
+
+    # Get query parameters
+    query_params = get_query_params()
+
+    if len(query_params) > 0:
+        # Remove all query parameters from URL
+        st.experimental_set_query_params()
+        st.session_state["form_defaults"] = deep_update(
+            st.session_state["form_defaults"], query_params
+        )
+
+    default_tab = (
+        query_params["method"]
+        if "method" in query_params.keys()
+        else st.session_state["form_defaults"]["method"]
+    )
 
     # Create tab bar to select method for location input
     active_tab = stx.tab_bar(
@@ -414,14 +664,15 @@ with col1:
                 description="Location by coordinates",
             ),
         ],
-        default="by_name",
+        default=default_tab,
     )
 
-    # Build form based on selected tab
-    if active_tab == "by_name":
-        input_values = build_form(method="by_name")
-    elif active_tab == "by_coords":
-        input_values = build_form(method="by_coords")
+    if active_tab != st.session_state["form_defaults"]["method"]:
+        # Reset form defaults once method is changed
+        st.session_state["form_defaults"] = get_form_defaults()
+
+    # Build form
+    input_values = build_form(method=active_tab, params=query_params)
 
     # Create button to show random graph
     random_graph = st.button("Show random")
@@ -461,6 +712,10 @@ with col2:
                     st.pyplot(fig)
 
             st.write("")
+
+            with st.expander("Share graph"):
+                st.write("To share this graph, you can use the following URL:")
+                st.write(f"{st.session_state['share_url']}", unsafe_allow_html=True)
 
             with st.expander("Show map"):
                 with st.spinner("Creating map..."):
