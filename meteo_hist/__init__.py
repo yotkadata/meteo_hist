1c251ada451b0dae21fe575599ee014be642d62d
Jan Kühn <yotka@posteo.org>
2024-05-26 22:22:10 +0200

    Switch to Geopy for Nominatim

--- meteo_hist/__init__.py
+++ meteo_hist/__init__.py
@@ -2,6 +2,6 @@
 MeteoHist is a Python package to visualize historical weather data from OpenMeteo API.
 """
 
-from .exceptions import OpenMeteoAPIException
+from .exceptions import OpenMeteoAPIException, APICallFailed
 from .base import MeteoHist
 from .interactive import MeteoHistInteractive
